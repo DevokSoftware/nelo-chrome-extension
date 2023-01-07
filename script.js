07c0a1fb61e5acded9d450b8adfa2145865244a5
Leonel Goncalves <devok.software@gmail.com>
2023-01-07 01:22:21 +0100

    Integration with the backend to get movie recommendations

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // JavaScript for the extension
 
+class MovieRecommendation {
+  constructor(data) {
+    this.id = data.id;
+    this.externalId = data.externalId;
+    this.title = data.title;
+    this.originalTitle = data.originalTitle;
+    this.genreIds = data.genreIds;
+    this.overview = data.overview;
+    this.voteAverage = data.voteAverage;
+    this.releaseDate = data.releaseDate;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:8080/movie-recommendation/recommendation")
+    .then((response) => response.json())
+    .then((data) => {
+      const recommendation = new MovieRecommendation(data);
+
+      // Get the first card element
+      const card = document.querySelector(".card:first-of-type");
+
+      // Set the title and overview of the movie recommendation
+      card.querySelector("h1").textContent = recommendation.title;
+      card.querySelector("p").textContent = recommendation.overview;
+    });
   // Center the cards horizontally and vertically
   const cardContainer = document.querySelector(".card-container");
   cardContainer.style.display = "flex";
