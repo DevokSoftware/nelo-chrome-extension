243b075ec3a0b0243847404a8606e5f4a111de86
Leonel Goncalves <devok.software@gmail.com>
2023-01-14 01:44:12 +0100

    added search movie card

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 class MovieRecommendation {
   constructor(data) {
-    this.id = data.id;
-    this.externalId = data.externalId;
-    this.title = data.title;
-    this.originalTitle = data.originalTitle;
-    this.genreIds = data.genreIds;
-    this.overview = data.overview;
-    this.voteAverage = data.voteAverage;
-    this.releaseDate = data.releaseDate;
+    this.id = data.movie.id;
+    this.externalId = data.movie.externalId;
+    this.title = data.movie.title;
+    this.originalTitle = data.movie.originalTitle;
+    this.genreIds = data.movie.genreIds;
+    this.overview = data.movie.overview;
+    this.voteAverage = data.movie.voteAverage;
+    this.releaseDate = data.movie.releaseYear;
+    this.criteria = data.criteria;
   }
 }
 
@@ -35,4 +36,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Set the height of the extension to the full height of the window
   cardContainer.style.height = `${window.innerHeight}px`;
+});
+
+function searchMovie(e) {
+  console.log("key", e.keyCode);
+  if (e.keyCode === 13) {
+    e.preventDefault(); // Ensure it is only this code that runs
+
+    alert("Enter was pressed was presses");
+  }
+}
+
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+
+searchInput.addEventListener("keydown", async (event) => {
+  if (event.code === "Enter") {
+    // 13 is the keyCode for the Enter key
+    event.preventDefault();
+    const searchTerm = searchInput.value;
+    const response = await fetch(
+      `http://localhost:8080/movie-recommendation/search?title=${searchTerm}`
+    );
+    const data = await response.json();
+
+    data.forEach((movie) => {
+      const movieElement = document.createElement("div");
+      movieElement.classList.add("movie");
+      movieElement.innerHTML = `<p>${movie.title} (${movie.releaseYear})</p>`;
+      movieElement.addEventListener("click", async () => {
+        const movieId = movie.externalId;
+        const response = await fetch(
+          "http://localhost:8080/movie-recommendation/watched",
+          {
+            method: "POST",
+            body: JSON.stringify({ externalId: movieId }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+      });
+      searchResults.appendChild(movieElement);
+    });
+  }
 });
