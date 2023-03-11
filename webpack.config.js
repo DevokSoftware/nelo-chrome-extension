3511c7514f78508b8b4f01c4329b3300e7f65a49
Leonel Goncalves <devok.software@gmail.com>
2023-03-11 02:21:29 +0100

    Started migration from JS to React - Project Structure Only

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    newTab: path.resolve("src/index.tsx"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: { noEmit: false },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "content.js",
+    path: path.resolve(__dirname, "extension"),
+  },
+  plugins: [...getHtmlPlugins(["newTab"])],
+};
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: "React Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
